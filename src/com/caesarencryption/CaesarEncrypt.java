211aa5030c6077eeffdf1dba121c0434f6d6597d
Syr7-s <isasayar7@github.com>
2020-12-11 14:54:18 +0300

    CaesarEncrypted.java added 2 methods and first trial did.

--- src/com/caesarencryption/CaesarEncrypt.java
+++ src/com/caesarencryption/CaesarEncrypt.java
@@ -2,11 +2,19 @@ package com.caesarencryption;
 
 public class CaesarEncrypt {
     private String text="Hello Word";
+    private char[] encryptText;
+    protected  void preProcessing(){
+        text=text.replace(" ","");
+    }
     protected void caesarEncriyption(){
         char[] textChar=text.toCharArray();
+        encryptText=new char[textChar.length];
         for (int i=0; i<textChar.length;i++){
             int charNumberValue=(int)textChar[i];
-            System.out.println(textChar[i]+" : "+charNumberValue);
+            encryptText[i]=(char)(charNumberValue+3);
+            System.out.println(textChar[i]+" : "+charNumberValue+" --> "+encryptText[i]+" :"+((int)encryptText[i]));
         }
+        String caesarEncryptedText=String.valueOf(encryptText);
+        System.out.println(caesarEncryptedText);
     }
 }
--- src/com/caesarencryption/Main.java
+++ src/com/caesarencryption/Main.java
@@ -2,6 +2,8 @@ package com.caesarencryption;
 
 public class Main {
     public static void main(String[] args) {
-
+        CaesarEncrypt caesarEncrypt=new CaesarEncrypt();
+        caesarEncrypt.preProcessing();
+        caesarEncrypt.caesarEncriyption();
     }
 }
