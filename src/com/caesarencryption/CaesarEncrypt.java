9ae12247e270e95d7d549a7996460aae5c2e7547
Syr7-s <isasayar7@github.com>
2020-12-11 15:22:37 +0300

    .xml, CaesarEncrypt.java and Main.java updated.

--- src/com/caesarencryption/CaesarEncrypt.java
+++ src/com/caesarencryption/CaesarEncrypt.java
@@ -1,13 +1,15 @@
 package com.caesarencryption;
 
 public class CaesarEncrypt {
-    private String text="Hello Word";
+    private  String text;
     private char[] encryptText;
     private char[] turkishAlphabet = {'ğ', 'ı', 'ü', 'ö', 'ç', 'Ğ', 'İ', 'Ü', 'Ö', 'Ç'};
     private char[] latinAlphabet = {'g', 'i', 'u', 'o', 'c', 'G', 'I', 'U', 'O', 'C'};
-    protected  void preProcessing(){
+    int charNumberValue;
+    protected  void preProcessing(String textOld){
+        this.text=textOld;
         text=text.replace(" ","");
-        text = text.replaceAll("\\p{Punct}", "");
+        text = text.replaceAll("\\p{Punct}|[0-9]", "");
         text = text.trim();
         if (turkishAlphabet.length==latinAlphabet.length){
             for (int i=0; i<turkishAlphabet.length;i++){
@@ -19,9 +21,22 @@ public class CaesarEncrypt {
         char[] textChar=text.toCharArray();
         encryptText=new char[textChar.length];
         for (int i=0; i<textChar.length;i++){
-            int charNumberValue=(int)textChar[i];
-            encryptText[i]=(char)(charNumberValue+3);
-            //System.out.println(textChar[i]+" : "+charNumberValue+" --> "+encryptText[i]+" :"+((int)encryptText[i]));
+            if (textChar[i]=='x'){
+                encryptText[i]='a';
+                System.out.println(textChar[i]+" --> "+encryptText[i]+" :"+((int)encryptText[i]));
+            }else if(textChar[i]=='y'){
+                encryptText[i]='b';
+                System.out.println(textChar[i]+" --> "+encryptText[i]+" :"+((int)encryptText[i]));
+            }
+            else if(textChar[i]=='z'){
+                encryptText[i]='c';
+                System.out.println(textChar[i]+" --> "+encryptText[i]+" :"+((int)encryptText[i]));
+            }
+           else{
+                charNumberValue=(int)textChar[i];
+                encryptText[i]=(char)(charNumberValue+3);
+                System.out.println(textChar[i]+" : "+charNumberValue+" --> "+encryptText[i]+" :"+((int)encryptText[i]));
+            }
         }
         String caesarEncryptedText=String.valueOf(encryptText);
         System.out.println(caesarEncryptedText);
--- src/com/caesarencryption/Main.java
+++ src/com/caesarencryption/Main.java
@@ -1,9 +1,14 @@
 package com.caesarencryption;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         CaesarEncrypt caesarEncrypt=new CaesarEncrypt();
-        caesarEncrypt.preProcessing();
+        Scanner reader=new Scanner(System.in);
+        System.out.print("Enter Encrypted Text: ");
+        String text=reader.nextLine();
+        caesarEncrypt.preProcessing(text);
         caesarEncrypt.caesarEncriyption();
     }
 }
